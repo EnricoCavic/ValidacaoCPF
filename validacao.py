2f9affdc6c10e90b4f527a161f4018d435773792
Enrico Cavicchioli <enricocavic@gmail.com>
2024-01-15 22:50:59 -0300

    Update validacao.py

--- validacao.py
+++ validacao.py
@@ -19,27 +19,46 @@ def Calculo(digitos : list):
     # Se o resultado > 9 ele deve ser zero
     # Else o resultado não é alterado
     digito_gerado = 0 if sobra > 9 else sobra
-    return digito_gerado
-
-# Primeiro digito
+    return str(digito_gerado)
 
 clear()
-# Coletar a soma dos 9 primeiros digitos do cpf
-cpf_input = input('Digite um cpf:')
+# Primeiro digito
+while(True):
+    # Coletar a soma dos 9 primeiros digitos do cpf
+    cpf_input = input('Digite [exit] ou [e] para sair\nDigite um CPF:')
 
-cpf_input = cpf_input.replace('.', '')
-cpf_separado = cpf_input.split('-')
+    cpf_input = cpf_input.replace('.', '')
+    cpf_separado = cpf_input.split('-')
+    try: 
+        for cpf in cpf_separado:
+            int(cpf)
+    except:
+        casefold_input = cpf_input.casefold()
+        if(casefold_input == 'e' or casefold_input == 'exit'):
+            break
 
-try: 
-    for cpf in cpf_separado:
-        int(cpf)
-except:
-    print('Isso não é um número')
+        print('Isso não é um número válido.')
+        continue
 
-print(*cpf_separado)
-digitos_iniciais = list(cpf_separado[0])
+    cpf_completo = cpf_separado[0] + cpf_separado[1]
 
-primeiro_digito = str(Calculo(digitos_iniciais))
-digitos_iniciais.append(primeiro_digito)
-segundo_digito = Calculo(digitos_iniciais)
-print(primeiro_digito, segundo_digito)
+    #print(*cpf_separado)
+    cpf_final = list(cpf_separado[0])
+
+    primeiro_digito = Calculo(cpf_final)
+    cpf_final.append(primeiro_digito)
+
+    segundo_digito = Calculo(cpf_final)
+    cpf_final.append(segundo_digito)
+
+    cpf_final = ''.join(cpf_final)
+
+    if(cpf_completo != cpf_final):
+        print('CPF Inválido!',
+               f'CPF digitado:{cpf_completo}',
+               f'CPF calculado:{cpf_final}', sep='\n')
+    else:
+        print('CFP digitado é valido!')
+
+
+
